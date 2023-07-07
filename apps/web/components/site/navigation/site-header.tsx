3eb3645a23b61a08456c6d2fec9613811255591f
keonhee Lee <2kunhee94@gmail.com>
2023-07-07 18:32:18 +0900

    add linkedIn icon

--- apps/web/components/site/navigation/site-header.tsx
+++ apps/web/components/site/navigation/site-header.tsx
@@ -1,4 +1,9 @@
+import { Icons } from '@/components/icons';
 import MainNav from '@/components/site/navigation/main-nav';
+import { buttonVariants } from '@/components/ui/button';
+import { siteConfig } from '@/config/site';
+import { cn } from '@/lib';
+import Link from 'next/link';
 import MobileNav from './mobile-nav';
 
 export default function SiteHeader() {
@@ -7,6 +12,47 @@ export default function SiteHeader() {
       <div className='container flex h-14 items-center'>
         <MainNav />
         <MobileNav />
+        <div className='flex flex-1 items-center justify-between space-x-2 sm:space-x-4 md:justify-end'>
+          <div className='w-full flex-1 md:w-auto md:flex-none'></div>
+          <nav className='flex items-center space-x-1'>
+            <Link
+              href={siteConfig.links.github}
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div
+                className={cn(
+                  buttonVariants({
+                    size: 'sm',
+                    variant: 'ghost',
+                  }),
+                  'w-9 px-0 '
+                )}
+              >
+                <Icons.gitHub className='h-5 w-5' />
+                <span className='sr-only'>GitHub</span>
+              </div>
+            </Link>
+            <Link
+              href={siteConfig.links.linkedin}
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div
+                className={
+                  (buttonVariants({
+                    size: 'sm',
+                    variant: 'ghost',
+                  }),
+                  'w-9 px-0')
+                }
+              >
+                <Icons.linkedIn className='h-5 w-5 fill-current' />
+                <span className='sr-only'>Twitter</span>
+              </div>
+            </Link>
+          </nav>
+        </div>
       </div>
     </header>
   );
