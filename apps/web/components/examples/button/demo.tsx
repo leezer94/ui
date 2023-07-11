080ac85a8c1ab8a793b870e1c3242b0245e49f2f
keonhee Lee <2kunhee94@gmail.com>
2023-07-11 18:10:05 +0900

    comment : add todo comment for button demo component

--- apps/web/components/examples/button/demo.tsx
+++ apps/web/components/examples/button/demo.tsx
@@ -23,6 +23,8 @@ const buttons: Button[] = [
   { variant: 'disabled' },
 ];
 
+// TODO: 버튼 사이즈 조절이 가능한 컴포넌트 혹은 공통으로 사용 가능한 컴포넌트가 데모 페이지에 공통으로 필요
+
 export default function ButtonDemo() {
   return (
     <div className='flex flex-col'>
