d46cfa8c038d2e6d390168cc4b70aac2056b9bc4
keonhee Lee <2kunhee94@gmail.com>
2023-07-17 03:15:01 +0900

    add progress bar component

---
+++ apps/web/app/examples/feed/components/progress-bar.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { Progress } from '@/components/ui/progress';
+import { useEffect, useState } from 'react';
+
+interface Props {
+  percentage: number;
+  defaultProgress: number;
+}
+
+export default function ProgressBar({
+  percentage,
+  defaultProgress = 13,
+}: Props) {
+  const [progress, setProgress] = useState(defaultProgress);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setProgress(percentage), 500);
+    return () => clearTimeout(timer);
+  }, [percentage]);
+
+  return <Progress value={progress} className='w-[100%]' />;
+}
