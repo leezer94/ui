04e58087ef71c73b7fcdd30dacb600f8f199e899
keonhee Lee <2kunhee94@gmail.com>
2023-07-20 14:08:51 +0900

    add favicon / metatags

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,55 @@
 import '../styles/global.css';
 import { SiteFooter, SiteHeader } from '@/components/site/navigation';
+import type { Metadata } from 'next';
+import { siteConfig } from '@/config/site';
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    'Next.js',
+    'React',
+    'Tailwind CSS',
+    'Server Components',
+    'Radix UI',
+  ],
+  authors: [
+    {
+      name: 'leezer',
+      url: 'https://githb.com/leezer94',
+    },
+  ],
+  creator: 'shadcn',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  icons: {
+    icon: './favicon.ico',
+    shortcut: './favicon-16x16.png',
+    apple: './apple-touch-icon.png',
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+};
 
 export default function RootLayout({
   children,
