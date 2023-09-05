ca15344fe3033695449461fe636df962413a1839
keonhee Lee <2kunhee94@gmail.com>
2023-09-05 15:28:55 +0900

    add checkbox demo

--- apps/web/components/examples/checkbox/demo.tsx
+++ apps/web/components/examples/checkbox/demo.tsx
@@ -1,7 +1,72 @@
 'use client';
 
-import { Checkbox } from 'ui/components/checkbox';
+import { CheckIcon } from 'lucide-react';
+import { Checkbox, CheckboxIndicator } from 'ui/components/checkbox';
+import { Button } from 'ui/components/button';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+import { ZodType } from 'zod';
+import { useForm } from 'react-hook-form';
+
+interface FormType {
+  'terms-and-conditions': boolean;
+  'additional-services': boolean;
+}
 
 export default function CheckboxDemo() {
-  return <Checkbox></Checkbox>;
+  const schema: ZodType<FormType> = z.object({
+    'terms-and-conditions': z.boolean(),
+    'additional-services': z.boolean(),
+  });
+
+  const { register, handleSubmit } = useForm<FormType>({
+    resolver: zodResolver(schema),
+  });
+
+  const submitForm = (data: FormType) => {};
+
+  return (
+    <form
+      className='flex flex-col items-center justify-center'
+      onSubmit={handleSubmit(submitForm)}
+    >
+      <div className='mb-10 flex flex-col gap-y-2'>
+        <div className='flex flex-col'>
+          <div className='flex items-center'>
+            <Checkbox
+              id='terms-and-conditions'
+              {...register('terms-and-conditions')}
+            >
+              <CheckboxIndicator icon={<CheckIcon className='p-1' />} />
+            </Checkbox>
+            <label
+              className='select-none pl-[15px] text-[15px] leading-none text-black'
+              htmlFor='terms-and-conditions'
+            >
+              회원가입 개인정보 수집 및 이용 동의
+            </label>
+          </div>
+        </div>
+        <div className='flex items-center'>
+          <Checkbox
+            id='additional-services'
+            {...register('additional-services')}
+          >
+            <CheckboxIndicator icon={<CheckIcon className='p-1' />} />
+          </Checkbox>
+          <label
+            className='select-none pl-[15px] text-[15px] leading-none text-black'
+            htmlFor='additional-services'
+          >
+            부가서비스 이용 동의
+          </label>
+        </div>
+      </div>
+      <div>
+        <Button type='submit' variant='outline' size='sm'>
+          제출하기
+        </Button>
+      </div>
+    </form>
+  );
 }
--- packages/ui/components/checkbox.tsx
+++ packages/ui/components/checkbox.tsx
@@ -1,7 +1,43 @@
 'use client';
 
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import React, { ReactNode } from 'react';
+import { cn } from '../lib/utils';
 
-const Checkbox = CheckboxPrimitive.Root;
+interface CheckboxIndicatorProps {
+  icon: ReactNode;
+}
 
-export { Checkbox };
+const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      'hover:bg-foreground/5 flex h-[25px] w-[25px] border appearance-none items-center justify-center rounded-md bg-white outline-none ',
+      className
+    )}
+    {...props}
+  />
+));
+
+Checkbox.displayName = CheckboxPrimitive.Checkbox.displayName;
+
+const CheckboxIndicator = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Indicator>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Indicator> &
+    CheckboxIndicatorProps
+>(({ className, icon, ...props }, ref) => (
+  <CheckboxPrimitive.Indicator
+    ref={ref}
+    className={cn('text-black ', className)}
+    {...props}
+  >
+    {icon}
+  </CheckboxPrimitive.Indicator>
+));
+
+CheckboxIndicator.displayName = CheckboxPrimitive.Indicator.displayName;
+
+export { Checkbox, CheckboxIndicator };
